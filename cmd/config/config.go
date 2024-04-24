0a8775af5c8886e3eae13fc486d2ca1f1182fd77
aricamargo_meli <dario.camargo@mercadolivre.com>
2024-04-24 00:04:51 -0300

    Feature: Config using Viper

---
+++ cmd/config/config.go
@@ -0,0 +1,81 @@
+package config
+
+import (
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	configName   = "monitor-creator"
+	configPrefix = "monitor-creator"
+)
+
+var (
+	instance             *ConfigManager
+	once                 sync.Once
+	monitorCreatorConfig MonitorCreatorConfig
+)
+
+type ConfigManager struct {
+	viper *viper.Viper
+}
+
+func Initialize() *ConfigManager {
+	once.Do(func() {
+		instance = &ConfigManager{
+			viper: viper.New(),
+		}
+	})
+	return instance
+}
+
+func Get() MonitorCreatorConfig {
+	return monitorCreatorConfig
+}
+
+func LoadMonitorCreatorConfig() {
+	Initialize()
+
+	viper.SetEnvPrefix(configPrefix)
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.SetConfigName(configName)
+	viper.AddConfigPath("../config")
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	monitorCreatorConfig = MonitorCreatorConfig{
+		LogLevel: viper.GetString("log_level"),
+		Auth: AuthConfig{
+			Kind:       viper.GetString("auth.kind"),
+			Middleware: viper.GetString("auth.middleware"),
+		},
+		ClientAuth: HTTPClientCredential{
+			Token:    os.Getenv("FURY_TOKEN"),
+			ClientID: os.Getenv("SECRET_CLIENT_ID"),
+			SecretID: os.Getenv("SECRET_SECRET_ID"),
+		},
+	}
+}
+
+type MonitorCreatorConfig struct {
+	LogLevel   string
+	Auth       AuthConfig
+	ClientAuth HTTPClientCredential
+	DatadogURL string
+}
+
+type AuthConfig struct {
+	Kind       string
+	Middleware string
+}
+
+type HTTPClientCredential struct {
+	Token    string
+	ClientID string
+	SecretID string
+}
--- cmd/handlers/router.go
+++ cmd/handlers/router.go
@@ -1,9 +1,13 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/melisource/fury_go-core/pkg/web"
+	"github.com/melisource/fury_go-platform/pkg/fury"
 )
 
 func NewHTTPServer(h MonitorHandler) error {
@@ -24,4 +28,29 @@ func NewHTTPServer(h MonitorHandler) error {
 
 	// return http.ListenAndServe(port, r)
 	return r.Run(port)
+}
+
+func NewFuryApplication(handler *MonitorHandler) error {
+	app, err := fury.NewWebApplication()
+	fmt.Println(app)
+	if err != nil {
+		return err
+	}
+	app.Method(http.MethodPost, "/teste", func(w http.ResponseWriter, r *http.Request) error {
+		return web.EncodeJSON(w, "MEU OVO", http.StatusOK)
+	})
+	app.Get("/hello", func(w http.ResponseWriter, r *http.Request) error {
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			return web.NewError(http.StatusBadRequest, "param name cannot be empty")
+		}
+
+		type response struct {
+			Message string `json:"message"`
+		}
+
+		return web.EncodeJSON(w, response{"Hello " + name}, http.StatusOK)
+	})
+
+	return app.Run()
 }
